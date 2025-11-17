42cf272cfde3c9eb8ea35c10e61c7168fa8145cb
SkyBotsDeveloper <skybotsdeveloper@gmail.com>
2025-11-17 14:59:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/SkyBotsDeveloper/VivaanXmusic2.0",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
